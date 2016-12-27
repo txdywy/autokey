58f227d0d77ba37736a7bb9b68533efd7e13f0d7
Wei Yi <weiyi@papayamobile.com>
2016-12-27 11:09:46 +0800

    add wy to version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
       name="autokey",
-      version="0.91.1",
+      version="0.91.1-wy",
       author="Chris Dekter",
       author_email="cdekter@gmail.com",
       url="http://autokey.googlecode.com/",
